1f867258bfa6f7622aa5f0d33159d6cd0fbc3cde
jabuta <lfalvaz@gmail.com>
2024-03-10 21:09:59 -0500

    replicate static to public

--- src/cp_staic_public.py
+++ src/cp_staic_public.py
@@ -3,10 +3,20 @@ import shutil
 
 def init_public():
     try:
-        shutil.rmtree("../public")
+        shutil.rmtree("public/")
     except:
         pass
+    repl_static("static/")
     
-    os.mkdir("../public")
-    
-    
+def repl_static(curr_path: str):
+    if not os.path.exists(curr_path.replace("static","public",1)):
+        os.mkdir(curr_path.replace("static","public",1))
+
+    curr_files = os.listdir(curr_path)
+    print(curr_files)
+    for file in curr_files:
+        file_path = os.path.join(curr_path,file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path,file_path.replace("static","public",1))
+            continue
+        repl_static(file_path)
